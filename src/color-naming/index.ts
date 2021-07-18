8291fd52e2301eadaefc13dd0cbad7acc03681e6
uihyeon.cha <uihyeon.cha@gmail.com>
2021-07-18 19:52:28 +0900

    feat: :sparkles: color-naming 초안 작성

---
+++ src/color-naming/index.ts
@@ -0,0 +1,51 @@
+import * as vscode from 'vscode'
+import { camelCase, paramCase } from 'change-case'
+import { NameThatColor } from '@/ntc'
+import { Config } from '@/color-naming/interfaces'
+
+const ntc = new NameThatColor()
+
+export const getConfig = () => {
+    const config = vscode.workspace.getConfiguration('colorNaming')
+
+    return config as typeof config & Config
+}
+
+export const fromSelectedText = async () => {
+    const config = getConfig()
+    const editor = vscode.window.activeTextEditor
+
+    if (!editor) {
+        vscode.window.showInformationMessage('Editor does not exist')
+        return
+    }
+
+    const selectedText = editor.document.getText(editor.selection)
+    const match = ntc.getName(selectedText)
+
+    if (!match) {
+        vscode.window.showInformationMessage('No matching colors')
+        return
+    }
+
+    let convertedName: string
+    let [, colorName, difference] = match
+
+    if (difference) {
+        colorName += ` ${difference}`
+    }
+
+    switch (config.caseStyle) {
+        case 'Camel': {
+            convertedName = camelCase(colorName)
+            break
+        }
+        default: {
+            convertedName = paramCase(colorName)
+        }
+    }
+
+    await vscode.env.clipboard.writeText(convertedName)
+
+    vscode.window.showInformationMessage(`Copied color name: ${convertedName}`)
+}
---
+++ src/color-naming/interfaces.ts
@@ -0,0 +1,3 @@
+export interface Config {
+    caseStyle: 'Kebab' | 'Camel'
+}
